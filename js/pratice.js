2f4cf99a02a1d466ae2087b444cf353ea8472cc1
Shipu8644 <shipu.monjurur@gmail.com>
2021-08-27 15:27:48 +0600

    half-done

--- js/pratice.js
+++ js/pratice.js
@@ -1,1 +1,30 @@
-console.log(7777);
+const searchButton = () => {
+    const inputField = document.getElementById('input');
+    const inputText = inputField.value;
+    // console.log(inputText);
+    inputField.value = "";
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputText}`)
+        .then(res => res.json())
+        .then(data => loadData(data.meals))
+}
+
+const loadData = (meals) => {
+    // console.log(meals);
+    const sectionFood = document.getElementById('items');
+    sectionFood.textContent = '';
+    meals.forEach(meal => {
+        console.log(meal);
+
+        const div = document.createElement('div');
+        div.classList.add('container');
+        div.innerHTML = `<div>
+            <img src="${meal.strMealThumb}" alt="">
+                <h3>${meal.strMeal}</h3>
+                <div>
+                `
+        sectionFood.appendChild(div);
+
+    })
+
+}
